b7041d603bb9a9925d9fdbf256dad974b7b8fa4e
joeyklee <jsphknglee@gmail.com>
2019-08-07 10:58:48 -0400

    update faceapi and utils to check manifest array

--- scripts/download-models/FaceApi/index.js
+++ scripts/download-models/FaceApi/index.js
@@ -1,8 +1,8 @@
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
-const fs = require('fs');
-const mkdirp = require('mkdirp');
+const DownloaderUtils = require('../utils');
+const STORAGEPATH = "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi"
+const outputFolder = './models/faceapi';
 
+const downloaderUtils = new DownloaderUtils(STORAGEPATH, outputFolder);
 
 // https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/ssd_mobilenetv1_model-shard1
 // https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/ssd_mobilenetv1_model-shard2
@@ -13,44 +13,46 @@ const mkdirp = require('mkdirp');
 // https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/face_recognition_model-weights_manifest.json
 // https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/face_expression_model-weights_manifest.json
 async function downloadFaceApi(){
-    const STORAGEPATH = "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi"
-    const outputFolder = './models/faceapi';
+    
     // NOTE: paths are relative to where the script is being called
-    mkdirp.sync(outputFolder);
+    downloaderUtils.makeOutputPath();
 
-    await getWeights(STORAGEPATH, outputFolder, 'ssd_mobilenetv1_model-weights_manifest.json')
-    await getWeights(STORAGEPATH, outputFolder, 'face_landmark_68_model-weights_manifest.json')
-    await getWeights(STORAGEPATH, outputFolder, 'face_landmark_68_tiny_model-weights_manifest.json')
-    await getWeights(STORAGEPATH, outputFolder, 'face_recognition_model-weights_manifest.json')
-    await getWeights(STORAGEPATH, outputFolder, 'face_expression_model-weights_manifest.json')
+    
+    const a = await downloaderUtils.saveJson('ssd_mobilenetv1_model-weights_manifest.json')
+    await downloaderUtils.saveWeights(a);
+    
+    const b = await downloaderUtils.saveJson('face_landmark_68_model-weights_manifest.json')
+    await downloaderUtils.saveWeights(b);
+    
+    const c = await downloaderUtils.saveJson('face_landmark_68_tiny_model-weights_manifest.json')
+    await downloaderUtils.saveWeights(c);
+    
+    const d = await downloaderUtils.saveJson('face_recognition_model-weights_manifest.json')
+    await downloaderUtils.saveWeights(d);
+    
+    const e = await downloaderUtils.saveJson('face_expression_model-weights_manifest.json')
+    await downloaderUtils.saveWeights(e);
+
+
 
 }
 
-async function getWeights(STORAGEPATH, outputFolder, jsonName){
-    let modelJson = await fetch(`${STORAGEPATH}/${jsonName}`)
-    modelJson = await modelJson.json();
 
-    fs.writeFile(`${outputFolder}/${jsonName}`, JSON.stringify(modelJson), () => {
-        console.log('finished writing:', jsonName)
-    });    
+// async function getWeights(modelJson){
+    
+//     Promise.all(
+//         modelJson[0].paths.map( async (fileName) => {
+//             let partUrl = `${STORAGEPATH}/${fileName}`;
 
-    await getShards(STORAGEPATH, outputFolder, modelJson);
-}
+//             let shard = await fetch(partUrl);
+//             shard = await shard.buffer();
 
-async function getShards(STORAGEPATH, outputFolder, modelJson){
-    Promise.all(
-        modelJson[0].paths.map( async (fileName) => {
-            let partUrl = `${STORAGEPATH}/${fileName}`;
-
-            let shard = await fetch(partUrl);
-            shard = await shard.buffer();
-
-            fs.writeFile(`${outputFolder}/${fileName}`, shard, () => {
-                console.log('finished writing: ', fileName)
-            });
-        })
-    )
-}
+//             fs.writeFile(`${outputFolder}/${fileName}`, shard, () => {
+//                 console.log('finished writing: ', fileName)
+//             });
+//         })
+//     )
+// }
 
 
 module.exports = downloadFaceApi
--- scripts/download-models/utils/index.js
+++ scripts/download-models/utils/index.js
@@ -4,18 +4,18 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 
 class DownloaderUtils {
-    constructor(STORAGEPATH, OUTPUTFOLDER){
+    constructor(STORAGEPATH, OUTPUTFOLDER) {
         this.STORAGEPATH = STORAGEPATH;
         this.OUTPUTFOLDER = OUTPUTFOLDER;
     }
 
-    makeOutputPath(OTHERFOLDER){
+    makeOutputPath(OTHERFOLDER) {
         const oPath = OTHERFOLDER || this.OUTPUTFOLDER;
         mkdirp.sync(oPath);
         console.log(`Output directory created at: ${oPath}`);
     }
 
-    async saveJson(jsonFileWithPath){
+    async saveJson(jsonFileWithPath) {
         let modelJson;
         // jsonFileWithPath should be `model.json` or `manifest.json` or similar
         modelJson = await fetch(`${this.STORAGEPATH}/${jsonFileWithPath}`)
@@ -28,24 +28,43 @@ class DownloaderUtils {
         return modelJson;
     }
 
-    async saveWeights(modelJson){
-        if(!modelJson.hasOwnProperty('weightsManifest')) {
-            console.log("no weights manifest property found! - check your data");
+    async saveWeights(modelJson) {
+        if (!modelJson.hasOwnProperty('weightsManifest')) {
+
+            console.log("no weightsManifest property found - checking modelJson for path property");
+            // this is the case for faceapi
+            if (Array.isArray(modelJson) === true) {
+
+                modelJson.forEach(weights => {
+
+                    if (!weights.hasOwnProperty('paths')) return;
+                    // get an array of promises of the weights
+                    const weightsPromiseArray = weights.paths.map((fileName) => this.saveWeight(fileName));
+
+                    // call and wait for all those promises to finish
+                    Promise.all(weightsPromiseArray)
+
+                })
+                return;
+            }
+
+            console.log("no weightsManifest property found and no paths property! - check your model.json or manifest.json");
             return;
+
         }
-        
+
         // loop through each of the weights objects in the weightsManifest
-        modelJson.weightsManifest.forEach( (weights) => {
-            
+        modelJson.weightsManifest.forEach((weights) => {
+
             // get an array of promises of the weights
-            const weightsPromiseArray = weights.paths.map( (fileName) => this.saveWeight(fileName) );
-            
+            const weightsPromiseArray = weights.paths.map((fileName) => this.saveWeight(fileName));
+
             // call and wait for all those promises to finish
             Promise.all(weightsPromiseArray)
         })
     }
 
-    async saveWeight(fileName){
+    async saveWeight(fileName) {
         let weightFile;
         const weightUrl = `${this.STORAGEPATH}/${fileName}`;
 
